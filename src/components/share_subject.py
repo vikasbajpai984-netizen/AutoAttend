d2d908b4fdf24f2ed572c513fb615d1ef1b8a3f5
Vikas Bajpai <vikasbajpai984@gmail.com>
2026-08-29 16:32:27 +0530

    last

--- src/components/share_subject.py
+++ src/components/share_subject.py
@@ -5,7 +5,7 @@ import io
 @st.dialog("Share class link")
 def share_subject(sub_name, sub_code):
     
-    app_domain = "AutoAttend-main.streamlit.app"
+    app_domain = "autoattend-main.streamlit.app"
     join_url = f"{app_domain}/?join-code={sub_code}"
     
     st.header("Scan to join")
